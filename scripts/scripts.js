eabb3812749d38e8b7a63ef6e5e19c7d6438a734
Andreas Haller <andreas.haller@netcentric.biz>
2024-04-23 11:53:29 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -132,4 +132,15 @@ async function loadPage() {
   loadDelayed();
 }
 
+// Side-effects
+(async function daPreview() {
+  const { searchParams } = new URL(window.location.href);
+  const daPreview = searchParams.get('dapreview');
+  if (daPreview) {
+    const origin = daPreview === 'local' ? 'http://localhost:3000' : 'https://da.live';
+    const { default: livePreview } = await import(`${origin}/scripts/dapreview.js`);
+    livePreview(loadPage);
+  }
+}());
+
 loadPage();
